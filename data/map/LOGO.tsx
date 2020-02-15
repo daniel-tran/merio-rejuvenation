68c3f623353b7341884540a1764bcb323f1b5a15
Daniel Tran <daniel.tran11@gmail.com>
2020-02-15 14:05:58 +1100

    Add more visual detail to the main menu

---
+++ data/map/LOGO.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="LOGO" tilewidth="640" tileheight="128" tilecount="1" columns="1">
+ <image source="../img/backdrop/LOGO.png" width="640" height="128"/>
+</tileset>
