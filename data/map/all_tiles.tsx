17ffcdf351af7bf79bde647b4d5ae1acb5e996fd
Daniel Tran <daniel.tran11@gmail.com>
2020-02-20 19:17:27 +1100

    Update tiles with more detail

--- data/map/all_tiles.tsx
+++ data/map/all_tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.1" name="all_tiles" tilewidth="32" tileheight="32" tilecount="64" columns="8">
- <image source="../img/all_tiles.png" width="256" height="256"/>
+<tileset version="1.2" tiledversion="1.3.1" name="all_tiles" tilewidth="32" tileheight="32" tilecount="4" columns="2">
+ <image source="../img/all_tiles.png" width="64" height="64"/>
 </tileset>
